900c04a5a09422525cf2ca19eb11933c3f2f4d2d
hector_papoulias <hector.papoulias@geocomply.com>
2023-11-03 22:06:46 +0200

    Get batch generator.

--- transaction_anomaly_detection/models/autoencoder/trainer.py
+++ transaction_anomaly_detection/models/autoencoder/trainer.py
@@ -9,6 +9,20 @@ from transaction_anomaly_detection.models.tools.early_stopping import EarlyStopp
 class AutoencoderTrainer:
     @torch.no_grad()
     @staticmethod
+    def _get_batch_generator(
+        t_dataset: torch.tensor,  # t_dataset shape: (n_records, n_cat_feautres + n_con_eatures)
+        n_batches: int,
+        sz_batch: int,
+        random_seed: Optional[int] = None,
+    ) -> Generator[torch.tensor, None, None]:
+        for i in range(n_batches):
+            start_idx = i * sz_batch
+            end_idx = (i + 1) * sz_batch
+            t_batch = t_dataset[start_idx:end_idx, :]
+            yield t_batch  # t_batch shape: (B, n_cat_feautres + n_con_eatures)
+
+    @torch.no_grad()
+    @staticmethod
     def _shuffle_dataset(t_dataset: torch.tensor) -> torch.tensor:
         shuffled_indices = torch.randperm(t_dataset.size(0))
         return t_dataset[shuffled_indices]
@@ -24,3 +38,7 @@ class AutoencoderTrainer:
         t_dataset_train = t_dataset[split_size:]
         t_dataset_val = t_dataset[:split_size]
         return t_dataset_train, t_dataset_val
+
+    @staticmethod
+    def _get_n_batches(n_records: int, sz_batch: int) -> int:
+        return (n_records + sz_batch - 1) // sz_batch
