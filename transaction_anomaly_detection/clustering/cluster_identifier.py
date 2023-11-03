8ef66c51c201c1b67902c009649630a2b38a1ed4
hector_papoulias <hector.papoulias@geocomply.com>
2023-11-04 00:02:22 +0200

    Format cluster output.

--- transaction_anomaly_detection/clustering/cluster_identifier.py
+++ transaction_anomaly_detection/clustering/cluster_identifier.py
@@ -6,6 +6,30 @@ import geopandas as gpd
 from sklearn.neighbors import BallTree
 class ClusterIdentifier:
 
+    @staticmethod
+    def _format_cluster_output(
+        gdf_transactions: gpd.GeoDataFrame,
+        cluster_distances: np.array,
+        cluster_indices: np.array,
+    ) -> Tuple[pd.DataFrame, List[gpd.GeoDataFrame]]:
+        ls_clusters = []
+        dict_cluster_stats = defaultdict(list)
+        for i in range(len(cluster_indices)):
+            gdf_cluster = gdf_transactions.iloc[cluster_indices[i]]
+            n_spoofed = len(gdf_cluster[gdf_cluster["label"] == 1])
+            n_nonspoofed = len(gdf_cluster[gdf_cluster["label"] == 0])
+            class_balance = n_spoofed / (n_nonspoofed + n_spoofed)
+            dict_cluster_stats["dist_mean"].append(cluster_distances[i].mean())
+            dict_cluster_stats["dist_std"].append(cluster_distances[i].std())
+            dict_cluster_stats["n_spoofed"].append(n_spoofed)
+            dict_cluster_stats["n_nonspoofed"].append(n_nonspoofed)
+            dict_cluster_stats["class_balance"].append(class_balance)
+            ls_clusters.append(gdf_cluster)
+        df_cluster_stats = pd.DataFrame.from_dict(dict_cluster_stats)
+        df_cluster_stats["cluster_idx"] = df_cluster_stats.index
+        df_cluster_stats.sort_values(by="dist_mean", ascending=True, inplace=True)
+        return df_cluster_stats, ls_clusters
+
     @classmethod
     def _get_cluster_distances_indices(
         cls,
