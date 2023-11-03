c2e15bfa29cea017ce9cb5d2b0e0749d0deb0e26
hector_papoulias <hector.papoulias@geocomply.com>
2023-11-03 21:18:55 +0200

    Incorporate batch reduction to reconstruction loss.

--- transaction_anomaly_detection/models/layers/reconstruction_loss.py
+++ transaction_anomaly_detection/models/layers/reconstruction_loss.py
@@ -20,6 +20,9 @@ class ReconstructionLoss(nn.Module):
         # Shape: (B, n_features)
         t_con_targets: Optional[torch.tensor],
         # Shape: (B, n_features)
+        batch_reduction: Optional[str] = None
+        # Values include 'none', 'mean', 'sum'.
+        # Passing 'none' is equivalent to passing None.
     ) -> Tuple[Optional[torch.tensor], Optional[torch.tensor]]:
         t_cat_losses, t_con_losses = None, None
         if ls_cat_logits is not None and ls_cat_targets is not None:
@@ -31,4 +34,17 @@ class ReconstructionLoss(nn.Module):
             t_cat_losses = torch.stack(ls_cat_losses).t()
         if t_con_predictions is not None and t_con_targets is not None:
             t_con_losses = torch.pow(t_con_predictions - t_con_targets, 2)
+        if batch_reduction == "mean":
+            t_cat_losses = (
+                t_cat_losses.mean(dim=0) if t_cat_losses is not None else None
+            )
+            t_con_losses = (
+                t_con_losses.mean(dim=0) if t_con_losses is not None else None
+            )
+        if batch_reduction == "sum":
+            t_cat_losses = t_cat_losses.sum(dim=0) if t_cat_losses is not None else None
+            t_con_losses = t_con_losses.sum(dim=0) if t_con_losses is not None else None
         return t_cat_losses, t_con_losses
+
+    # Shape: (B, n_cat_features), (B, n_con_features) if no reduction,
+    # (n_cat_features), (n_con_features) otherwise.
