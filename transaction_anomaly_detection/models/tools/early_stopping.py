f5ea98d66769500f75bd3fb72ba900b785f65f51
hector_papoulias <hector.papoulias@geocomply.com>
2023-11-03 21:00:28 +0200

    Implement stopping condition evaluator.

--- transaction_anomaly_detection/models/tools/early_stopping.py
+++ transaction_anomaly_detection/models/tools/early_stopping.py
@@ -48,3 +48,47 @@ class EarlyStopper:
     @property
     def best_model(self) -> nn.Module:
         return self._best_model
+    @classmethod
+    def _stopping_condition_met(
+        cls,
+        patience: int,
+        delta_threshold: float,
+        max_n_epochs: int,
+        n_epochs_ellapsed: int,
+        ls_historic_metrics: List[float],
+    ) -> bool:
+        if n_epochs_ellapsed < 1 + patience:
+            return False
+        if n_epochs_ellapsed > max_n_epochs:
+            return True
+        ls_successive_metrics = cls._get_successive_metrics(
+            ls_historic_metrics=ls_historic_metrics, patience=patience
+        )
+        ls_deltas = cls._get_deltas(ls_successive_metrics=ls_successive_metrics)
+        ls_deltas_exceeded_threshold = cls._deltas_exceeded_threshold(
+            ls_deltas=ls_deltas, delta_threshold=delta_threshold
+        )
+        return not any(ls_deltas_exceeded_threshold)
+
+    @staticmethod
+    def _deltas_exceeded_threshold(
+        ls_deltas: List[float], delta_threshold: float
+    ) -> List[bool]:
+        return list(map(lambda delta: delta < -delta_threshold, ls_deltas))
+
+    @staticmethod
+    def _get_deltas(
+        ls_successive_metrics: List[Tuple[float, float]],
+    ) -> List[float]:
+        return list(map(lambda tup_xy: tup_xy[1] - tup_xy[0], ls_successive_metrics))
+
+    @staticmethod
+    def _get_successive_metrics(
+        ls_historic_metrics: List[float],
+        patience: int,
+    ) -> List[Tuple[float, float]]:
+        ls_historic_metrics = ls_historic_metrics[-(1 + patience) : :]
+        return [
+            (ls_historic_metrics[i], ls_historic_metrics[i + 1])
+            for i in range(len(ls_historic_metrics) - 1)
+        ]
