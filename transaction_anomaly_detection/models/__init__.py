a0a2dee321e0d428efda31f2624dbd2c0b31fddd
hector_papoulias <hector.papoulias@geocomply.com>
2023-11-07 13:52:42 +0200

    Expose TextEncoder.

--- transaction_anomaly_detection/models/__init__.py
+++ transaction_anomaly_detection/models/__init__.py
@@ -1,3 +1,6 @@
 from transaction_anomaly_detection.models.autoencoder.interface import (
     TransactionAnomalyDetector,
 )
+from transaction_anomaly_detection.models.text_encoder.interface import (
+    TextEncoder,
+)
