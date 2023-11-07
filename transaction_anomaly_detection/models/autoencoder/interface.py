Fix type handling in reconstruction loss computation.

transaction_anomaly_detection/models/autoencoder/interface.py
@@ -11,8 +11,6 @@ from transaction_anomaly_detection.models.autoencoder.trainer import Autoencoder
 class TransactionAnomalyDetector:
     def __init__(
         self,
-        dict_cat_feature_to_ls_categories_n_embd: Dict[str, Tuple[List[str], int]],
-        ls_con_features: List[str],
         encoder_layer_szs: List[int],
         ae_activation: nn.Module,
         dropout_rate: Optional[float] = 0,
@@ -191,7 +189,7 @@ class TransactionAnomalyDetector:
         self,
         input_data: Union[pd.Series, pd.DataFrame],
         average_over_features: Optional[bool] = False,
-    ) -> pd.Series:
+    ) -> Union[pd.Series, pd.DataFrame]:
         return self._compute_reconstruction_loss_by_record(
             autoencoder=self._autoencoder,
             input_data=input_data,
@@ -370,12 +368,12 @@ class TransactionAnomalyDetector:
     def _compute_reconstruction_loss_by_record(
         cls,
         autoencoder: Autoencoder,
-        input_data: pd.DataFrame,
+        input_data: Union[pd.Series, pd.DataFrame],
         ls_cat_features: List[str],
         ls_con_features: List[str],
         dict_cat_feature_to_tokenizer: Dict[str, Tokenizer],
         average_over_features: bool,
-    ) -> Union[pd.DataFrame, pd.Series]:
+    ) -> Union[pd.Series, pd.DataFrame]:
         t_input_data = cls._prepare_t_input(
             input_data=input_data,
             ls_cat_features=ls_cat_features,
@@ -397,7 +395,7 @@ class TransactionAnomalyDetector:
         )
         df_loss_by_record = cls._format_df_loss_by_record(
             dict_loss_by_feature=dict_loss_by_feature,
-            index=input_data.index.tolist(),
+            index=cls._extract_index(input_data=input_data),
         )
         if average_over_features:
             sr_loss_by_record = df_loss_by_record.mean(axis=1)